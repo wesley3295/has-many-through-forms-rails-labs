bb59469b813373fce047dd1999b3f2f8595c1c5c
Wesley <wspencer3295@yahoo.com>
2021-01-16 15:06:17 -0700

    commit tree 781d261d7fe287043086d6bfc583ea5e9bc1b096

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
   has_many :post_categories
   has_many :posts, through: :post_categories
+  accepts_nested_attributes_for :posts
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,14 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
 
-
-end
+    def categories_attributes=(category_attributes)
+      category_attributes.values.each do | value |
+        if value[:name].present?
+          category = Category.find_or_create_by(value)
+          self.categories << category
+        end
+      end
+    end
+  end
